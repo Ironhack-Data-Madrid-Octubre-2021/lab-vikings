0803c2cddd331eca49193f8faeb628400d669c92
mariadelascotrina <mariadelascotrina@hotmail.com>
2021-10-22 14:33:50 +0200

    prueba 1

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,6 +1,26 @@
 
 # Soldier
+print(hola)
+quetal
+y = x
+suma = x+y
+ovejasdormidas = [True,  True,  True,  False,
+  True,  True,  True,  True ,
+  True,  False, True,  False,
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
 
+  type(ovejasdormidas)
+
+def count_sheeps(sheep):
+    ovejaspresentes = 0
+    for oveja in sheep:
+        if oveja is True:
+            ovejaspresentes += 1
+    return ovejaspresentes
+
+def (hola(9))
 
 class Soldier:
     pass
