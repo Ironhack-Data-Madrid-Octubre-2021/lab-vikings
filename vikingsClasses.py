af1c77da3cf7b89265725fe8362bccf08dfcf544
baccandres <88410722+baccandres@users.noreply.github.com>
2021-10-25 08:17:48 +0200

    Update vikingsClasses.py

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -51,7 +51,44 @@ class Saxon(Soldier):
 
 # War
 
-
+import random
 class War:
-    def __init__
-    pass
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, viking): 
+        self.viking=viking
+        self.vikingArmy.append(self.viking)
+
+    def addSaxon(self, saxon): 
+        self.saxon=saxon
+        self.saxonArmy.append(self.saxon)
+
+    def vikingAttack(self):
+        a = random.choice(self.saxonArmy) 
+        b = random.choice(self.vikingArmy) 
+        z = a.receiveDamage(b.attack()) 
+
+        for i in self.saxonArmy:
+            if i.health <= 0:
+                self.saxonArmy.remove(i)
+        return z
+
+    def saxonAttack(self):
+        a = random.choice(self.vikingArmy)
+        b = random.choice(self.saxonArmy)
+        z = a.receiveDamage(b.attack())
+
+        for i in self.vikingArmy:
+            if i.health <= 0:
+                self.vikingArmy.remove(i)
+        return z
+
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        if len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
